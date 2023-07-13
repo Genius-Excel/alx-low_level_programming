07d6de22828102ac9f8af62b6c1b7d56f6cd8abf
Genius-Excel <geniusexcel9@gmail.com>
2023-07-13 12:57:07 -0700

    Puts functions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/*
+* Outputs something and runturns 0
+*/
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
