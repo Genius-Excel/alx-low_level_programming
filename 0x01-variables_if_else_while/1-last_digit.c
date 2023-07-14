ed7c4b528f5f7926d1e6f8e8efe13a6c14108d5e
Genius-Excel <geniusexcel9@gmail.com>
2023-07-14 12:55:41 -0700

    Last integer

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - this is the main function
+* Return:always 0
+*
+*/
+int main(void)
+{
+	int n, last_int;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_int = abs(n) % 10;
+	if (last_int == 0)
+	{
+		printf("Last digit of %d is %d and is 0 \n", n, last_int);
+	}
+	else if (last_int > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_int);
+	}
+	else if (last_int < 6 && last_int != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_int);
+	}
+	return (0);
+}
