8a0b4cefb309554a6737bed87086d04f03b885bb
Genius-Excel <geniusexcel9@gmail.com>
2023-07-14 14:31:29 -0700

    for loops

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*
+* main - this is the main function 
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	int lower , upper;
+
+	for (lower = 0; lower < 26; lower++)
+	{
+		putchar('a' + lower);
+	}
+	for (upper = 0; upper < 26; upper++)
+	{
+		putchar('A' + upper);
+	}
+	putchar('\n');
+	return (0);
+}
