5376e81dfd188d333c1e5d801d19813e4dac8bc0
Genius-Excel <geniusexcel9@gmail.com>
2023-07-14 14:08:46 -0700

    Insomnia? play with alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Return:always 0
+*/
+
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 0; alpha < 26; alpha++)
+	{
+		putchar('a' + alpha);
+	}
+
+	putchar('\n')
+	return (0);
+}
