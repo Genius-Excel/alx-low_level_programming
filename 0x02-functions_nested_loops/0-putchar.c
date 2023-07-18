ad7b27a9824fcd2414477fe8a8ada1b0662dbf56
Genius-Excel <geniusexcel9@gmail.com>
2023-07-18 09:06:03 -0700

    _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <unistd.h>
 /**
-* main - this is the main function
-* Retur:always 0
+* main - entry point this ouputs with _putchar
+* Return:always 0
 */
 int main(void)
 {
