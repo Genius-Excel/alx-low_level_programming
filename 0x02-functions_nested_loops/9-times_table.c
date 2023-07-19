938f9300c10655ef234f8c86d4e5592292e5eba0
Genius-Excel <geniusexcel9@gmail.com>
2023-07-18 18:29:16 -0700

    9-times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+* times_table - outputs table
+* Return: void
+*/
+
+void times_table(void)
+{
+	int rows, cols, val;
+
+	for (rows = 0; rows <= 9; rows++)
+	{
+		for (cols = 0; cols <= 9; cols++)
+		{
+			val = rows * cols;
+
+			if (cols != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (val <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((val / 10) + '0');
+			}
+			if (val <= 9)
+			{
+				_putchar(val + '0');
+			}
+			else
+			{
+				_putchar((val % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
