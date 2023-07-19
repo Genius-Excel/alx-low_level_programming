bf614eb6d7a78893d86de7d167c0ecf291a36f81
Genius-Excel <geniusexcel9@gmail.com>
2023-07-19 08:05:03 -0700

    Multiples of 3 & 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,17 +7,23 @@
 
 int main(void)
 {
-	int sum, num;
+	int num, mul3, mul5, sum;
 
-	sum = 0;
+	mul3, mul5, sum = 0;
 
 	for (num = 0; num < 1024; num++)
 	{
-		if ((num % 3 == 0) || (num % 5 == 0))
+		if (num % 3 == 0)
 		{
-			sum += num;
+			mul3 += num;
+		}
+		else if (num % 5 == 0)
+		{
+			mul5 += num;
 		}
 	}
+	sum = mul3 + mul5;
+
 	printf("%d \n", sum);
 	return (0);
 }
