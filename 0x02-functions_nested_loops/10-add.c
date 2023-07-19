1406b9f07500ae7c9cf807cb5e9ea50d13fd6046
Genius-Excel <geniusexcel9@gmail.com>
2023-07-18 18:46:15 -0700

    Add two nums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add - sums two num
+* @a:this is first num
+* @b:this is second num
+* Return:sum of numbers
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
