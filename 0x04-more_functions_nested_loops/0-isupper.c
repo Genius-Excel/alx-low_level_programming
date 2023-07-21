028d92ee4b36b3d3fc2efa4e920546c9c8d462aa
Genius-Excel <geniusexcel9@gmail.com>
2023-07-20 19:51:08 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase
+* @c: value to be checked
+* Return: 0 if upper, 1 otherwise
+*/
+
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else if (c == 'a')
+	{
+		return (0);
+	}
+}
