4eb99e6bebb3fb3aafda2124c912e01f1643bd0e
Genius-Excel <geniusexcel9@gmail.com>
2023-07-20 20:24:39 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - output numbers
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 9; num++)
+	{
+		_putchar('0' + num);
+	}
+	_putchar('\n');
+}
