9f52240a046df72d2b388859f0630d2ad388c6a2
Genius-Excel <geniusexcel9@gmail.com>
2023-07-20 20:02:04 -0700

    Is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - checks for num
+* @c : num to be checked
+* Return: 0 if !num, 1 otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
