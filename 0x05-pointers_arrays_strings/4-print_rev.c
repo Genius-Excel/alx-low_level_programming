773e73a2b7efab4708a4ae61851c56544c57a512
genius <geniusexcel9@gmail.com>
2023-07-24 21:52:22 +0100

    Output reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_rev - outputs the reverse of a string
+ * @s: string to be reversed
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+void print_rev(char *s)
+{
+
+	int length, idx;
+
+	length = _strlen(s);
+
+	for (idx = length - 1; idx >= 0; idx--)
+	{
+		_putchar(s[idx]);
+	}
+	_putchar('\n');
+}
