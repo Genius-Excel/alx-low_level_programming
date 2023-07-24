3356382425e3f5044c37e4cd30fba32c598d479b
genius <geniusexcel9@gmail.com>
2023-07-24 23:12:03 +0100

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * rev_string - outputs the string in reverse
+ * @s: string to be reversed
+ * Return: void
+ */
+
+int _strlen(char *c);
+
+void rev_string(char *s)
+{
+	int len, idx;
+	
+	len = _strlen(s);
+
+	for (idx = len - 1; idx >= 0; idx--)
+	{
+		_putchar(s[idx]);
+	}
+	_putchar('\n');
+
+}
+
+/**
+ * _strlen - this output the length of the string
+ * @s: string to be checked
+ * Return: len
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
