4827d43d3fac01852c27c6379a247619a045ce8b
genius <geniusexcel9@gmail.com>
2023-07-24 20:07:30 +0100

    Swap integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - changes the values of two num
+* @a: first number
+* @b: second number
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+    *a = 42;
+
+    *b = 98;
+}
