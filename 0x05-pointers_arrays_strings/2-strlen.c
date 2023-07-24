a72f188070d3758b21f4671ace4a02b7b9fa277f
genius <geniusexcel9@gmail.com>
2023-07-24 20:47:10 +0100

    Sting length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strlen - calculates the length of a string
+* @s: string o be checked
+* Return: len
+*/
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	do
+	{
+		len++;
+		s++;
+	}
+	while (*s != '\0');
+
+	return (len);
+}
