3fe5362a55f27e660e0b0e70c2796d3652465075
genius <geniusexcel9@gmail.com>
2023-07-25 02:03:56 +0100

    Print elements in an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - ouputs the elemets in an array
+ * @a: adrress of the element in array
+ * @n: lenght of array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int idx;
+
+	for (idx = 0; idx < n; idx++)
+	{
+		printf("%d", a[idx]);
+
+		if (idx < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
