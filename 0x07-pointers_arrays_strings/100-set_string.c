789a182281e4d22b29af879ad2f675680cb7b2be
Genius-Excel <geniusexcel9@gmail.com>
2023-07-31 11:51:18 -0700

    Double Pointer Double Fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* set_string - this function sets the value of a pointer to char
+* @s : string to be checked
+* @to: value character
+* Return: void
+*/
+
+void set_string(char **s, char *to)
+{
+	if (*s != NULL)
+	{
+		free(*s);
+	}
+
+	*s = (char*)malloc((strlen(to) + 1) * sizeof(char));
+
+	if (*s != NULL)
+	{
+		strcpy(*s, to);
+	}
+}
