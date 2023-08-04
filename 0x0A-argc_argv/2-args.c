a2c236dd064ce25decec1c2cdfe8899b1d6dba21
Genius-Excel <geniusexcel9@gmail.com>
2023-08-04 08:18:31 -0700

    Output command line argument and a new line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - this function printd the passed argument
+* @argc: command line argument count
+* @argv: command line argument vector array
+* Return: 0 for success
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc >= 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+		printf("\n");
+	}
+	return (0);
+}
