d509c4dd21cbe0e6899f39ee5a34c2bd27c8f8bd
Genius-Excel <geniusexcel9@gmail.com>
2023-08-14 10:26:59 -0700

    Struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+# Typedef and Struct in C
+* Creating a struct in c
+* Pointers to a struct
+* Derefrencing a pointer to a struct
+* typdef struct
+* and many more ...
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+
+/**
+* struct dog - this structure has attributes og a dog
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*
+* Description - prints the name age and owner
+*/
+
+struct dog
+	{
+		 char *name;
+		 float age;
+		 char *owner;
+	};
+
+#endif
