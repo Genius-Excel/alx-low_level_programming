ed6dd103a8b8cd0ebfe15f39491c1e382fc057e1
Genius-Excel <geniusexcel9@gmail.com>
2023-08-14 06:41:19 -0700

    Done with preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - this function prints the name of the file compiled from
+* Return: the name of the file which it was compiled from
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
