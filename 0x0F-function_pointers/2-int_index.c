92f1cba29b43db5400262463c29f88f25658708d
Genius-Excel <geniusexcel9@gmail.com>
2023-08-16 16:29:43 -0700

    Compare integers in an array and return the index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+#include <stdbool.h>
+
+/**
+* int_index - this function check for the index of an
+* integer element in array
+* @array: array of elements to be checked
+* @size: size of the array
+* @cmp: function to compare if element is an integer
+* Return: returns the index of the first element
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, num_index;
+
+	if (size <= 0 || array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		num_index = 0;
+
+		if (cmp(array[i]) != false)
+		{
+			num_index = i;
+			return (num_index);
+		}
+	}
+
+	return (-1);
+}
