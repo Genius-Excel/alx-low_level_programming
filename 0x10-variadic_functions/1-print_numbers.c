571c1d3d3cb92b3dc5761c44c82b9edd1125e92d
Genius-Excel <geniusexcel9@gmail.com>
2023-08-18 02:01:48 -0700

    Print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* print_numbers - this function prints numbers with a seperator
+* passed as argument
+* @seperator: seperator of the numbers
+* @n: number of integers to be printed
+* Return: void
+*/
+
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list numbers;
+
+	va_start(numbers, n);
+
+	if (seperator == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(numbers, int));
+
+		if ((i < n - 1) && (seperator != NULL))
+		{
+			printf("%s", seperator);
+		}
+	}
+
+	putchar('\n');
+}
