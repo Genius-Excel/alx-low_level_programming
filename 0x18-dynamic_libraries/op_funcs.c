f4a15710f5f9e77d7e12f181acfee939d16b427f
Genius-Excel <geniusexcel9@gmail.com>
2023-11-13 08:23:27 -0800

    Operations function C source file.

---
+++ 0x18-dynamic_libraries/op_funcs.c
@@ -0,0 +1,55 @@
+/**
+* add - this functone adds two integers.
+* @a: first number
+* @b: second number
+* Return: the sum of a and b.
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - this function subtracts two integers.
+* @a: firs number.
+* @b: second number.
+* Return: the subtraction of a and b.
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - this function multiplies two integers.
+* @a: first number.
+* @b: second number.
+* Return: the multiplication ofthe two integers.
+*/
+int mul(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* div - this function divides two integers.
+* @a: first number.
+* @b: second number.
+* Return: the division of the two integers.
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - this function calculates the modulo of two integers.
+* @a: first number.
+* @b: second number.
+* Return: the modulo of two integers.
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
