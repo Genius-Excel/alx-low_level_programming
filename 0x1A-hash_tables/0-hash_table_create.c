838f85892013a57b1025cdd97ebe87e240577390
Genius-Excel <geniusexcel9@gmail.com>
2023-12-01 15:45:36 -0800

    A new edge cse has been handled.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,6 +22,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
+	(*new_table).array = malloc(sizeof(hash_node_t *) * size);
+
+	if (new_table->array == NULL)
+	{
+		return (NULL);
+	}
+
 	(*new_table).size = size;
 
 	return (new_table);
