7848ab6bf62f2aa1c5bea13fcc8165ec2dc2e2b0
Genius-Excel <geniusexcel9@gmail.com>
2023-12-01 15:09:16 -0800

    This file contains a function that creates a new hash table.

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - this functon creates a new table based on size.
+ * @size: size of the hash table to be created.
+ * Return: pointer to the newly created hash table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+
+	new_table = malloc(sizeof(hash_table_t));
+
+	if (new_table == NULL)
+	{
+		return (NULL);
+	}
+
+	(*new_table).size = size;
+
+	return (new_table);
+}
